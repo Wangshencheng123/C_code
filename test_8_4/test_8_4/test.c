d6fd4fa044fefadb2642213dc71f2e4ed36cce7b
Wangshencheng123 <1904001142@qq.com>
2021-08-05 10:17:39 +0800

    test_8_4

---
+++ test_8_4/test_8_4/test.c
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	printf("abc\n");
+//	return 0;
+//}
+
+
+
+
+
+
+
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a', 'b', 'c', '\0' };//此时结果为3
+//	//char arr2[] = { 'a', 'b', 'c' };  此时结果为随机值
+//	printf("%d\n", strlen(arr1));//strlen--string length 用来计算字符串长度
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	//数据在计算机上存储的时候，存储的是2进制
+//	//’a' 97
+//	//‘A’65
+//	//ASCII码值
+//	char arr1[] = "abc";//数组
+//	//"abc"--'a', 'b', 'c', '\0'---'\0'字符串的结束标志
+//	char arr2[] = { 'a', 'b', 'c' ,'\0'};
+//	//'\0'--值为0
+//	//'a'--97,每个字符都有一个值
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+
+
+
+
+
+
+//int main()
+//{	
+//	//int float
+//	//char
+//	//字符串型
+//	"avdasg";
+//		  "";//空字符串
+//
+//	return 0;
+//}
